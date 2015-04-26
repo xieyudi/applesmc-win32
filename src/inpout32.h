f7a4ecd0c93ed135f12cc54b044f7ad54d32caad
Yudi Xie <xieyudi1990@gmail.com>
2015-04-25 21:16:26 -0400

    initial commit

--- src/inpout32.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-//Functions exported from DLL.
-//For easy inclusion is user projects.
-void	__stdcall Out32(short PortAddress, short data);
-short	__stdcall Inp32(short PortAddress);
